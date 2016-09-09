a257297d0015b76e730d23798d8bd59483370102
JUNG YONG WOO <stegano@naver.com>
2016-09-09 15:07:03 +0900

    바운더리 정보 생성

--- src/tableExport.js
+++ src/tableExport.js
@@ -1,115 +1,149 @@
-function TableExport(element, options) {
+var TableExport = function (element, options) {
 
     if (!(this instanceof TableExport)) {
         // new 생성자를 사용하여 생성하도록 강제한다.
         throw "new 생성자를 이용해 주세요 :(";
     }
 
-    if (!(element instanceof HTMLTableElement)) {
+    // 이 함수는 호이스팅 됨
+    function gather(element, options) {
 
-        throw "인자 값으로 입력되는 엘리먼트는 테이블 엘리먼트이어야만 합니다 :("
-    }
+        if (!(element instanceof HTMLTableElement)) {
 
-    var table = {
-        rows: [],
-        metadata: []
-    };
+            throw "인자 값으로 입력되는 엘리먼트는 테이블 엘리먼트이어야만 합니다 :("
+        }
 
-    // `options`에 들어간 기본 값을 생성한다.
-    options = !options ? {mergeCell: {}} : options;
-    options.mergeCell.fill = options.mergeCell.fill === undefined ? true : options.mergeCell.fill;
+        var ret = {
+            rows: [],
+            metadata: [],
+            boundary: {
+                horizontal: 0,
+                vertical: 0
+            }
+        };
 
-    // 테이블 생성
-    for (var i = 0, tr, trs = element.children, ilen = trs.length; tr = trs[i], i < ilen; i++) {
-        // `tr`
-        if (tr instanceof HTMLTableRowElement && tr.childElementCount > 0) {
+        // `options`에 들어간 기본 값을 생성한다.
+        options = !options ? {mergeCell: {}} : options;
+        options.mergeCell.fill = options.mergeCell.fill === undefined ? true : options.mergeCell.fill;
 
-            for (var k = 0, cSize = 0, cell, cells = tr.children, klen = cells.length; cell = cells[k], k < klen; k++) {
+        // 테이블 생성
+        for (var i = 0, tr, trs = element.children, ilen = trs.length; tr = trs[i], i < ilen; i++) {
+            // `tr`
+            if (tr instanceof HTMLTableRowElement && tr.childElementCount > 0) {
 
-                if (k === 0) {
-                    // 테이블의 컬럼 갯수를 센다.
-                    for (var j = 0, colspan; j < klen; j++) {
+                for (var k = 0, cSize = 0, cell, cells = tr.children, klen = cells.length; cell = cells[k], k < klen; k++) {
 
-                        if (colspan = Number(cell.getAttribute('colspan')) > 0) {
-                            cSize += colspan;
-                        } else {
-                            cSize++
+                    // `th`, `td`
+                    if (cell instanceof HTMLTableCellElement) {
+
+                        if (k === 0) {
+                            // 테이블의 컬럼 갯수를 센다.
+                            for (var j = 0, colspan; j < klen; j++) {
+
+                                if (colspan = cell.colspan > 0) {
+                                    cSize += colspan;
+                                } else {
+                                    cSize++
+                                }
+                            }
+                        }
+
+                        // 셀에 데이터가 존재하지 않는 경우 데이터를 입력한다. -- `colspan`, `rowspan`으로 인해 값이 미리 입력되는 경우를 제외한 모든 경우.
+                        var ctext = cell.innerText;
+
+                        if (!ret.rows[i]) {
+                            // `Row`가 존재하지 않을경우 생성
+                            ret.rows[i] = [];
+                            ret.metadata[i] = [];
+                        }
+
+                        ret.rows[i].push(ctext);
+                        // DOM 객체에 접근하지 않기 위해 미리 데이터를 생성함.
+                        ret.metadata[i].push({
+                            type: cell.nodeName,
+                            colspan: cell.colSpan,
+                            rowspan: cell.rowSpan
+                        });
+
+                        // 테이블 헤더의 경계선을 찾는다.
+                        if (k === klen - 1 && cell.nodeName === 'TH') {
+                            // 가로선의 경계를 찾는다
+                            ret.boundary.horizontal++;
+                        }
+
+                        if (i === ilen - 1 && cell.nodeName === 'TH') {
+                            // 세로선의 경계를 찾는다
+                            ret.boundary.vertical += cell.colSpan > 1 ? cell.colSpan : 1;
                         }
                     }
                 }
-                // `th`, `td`
-                if (cell instanceof HTMLTableCellElement) {
+            }
+        }
 
-                    // 셀에 데이터가 존재하지 않는 경우 데이터를 입력한다. -- `colspan`, `rowspan`으로 인해 값이 미리 입력되는 경우를 제외한 모든 경우.
-                    var ctext = cell.innerText;
+        // `colspan`, `rowspan` 데이터를 이용하여 빈 공간을 채움.
+        for (var i = 0; i < ret.metadata.length; i++) {
 
-                    if (!table.rows[i]) {
-                        // `Row`가 존재하지 않을경우 생성
-                        table.rows[i] = [];
-                        table.metadata[i] = [];
+            for (var k = 0; k < ret.metadata[i].length; k++) {
+
+                var fill = options.mergeCell.fill === true ? ret.rows[i][k] : options.mergeCell.fill;
+                var span = ret.metadata[i][k];
+
+                if (span.colspan > 1) {
+                    // `colspan` 개수만큼 셀에 데이터를 복사한다.
+                    var rtail = ret.rows[i].splice(k + 1);
+                    var mtail = ret.metadata[i].splice(k + 1);
+
+
+                    for (var j = 1; j < span.colspan; j++) {
+
+                        ret.rows[i].push(fill);
+                        ret.metadata[i].push({type: ret.rows[i][k].nodeName, colspan: 0, rowspan: 0});
                     }
 
-                    table.rows[i].push(ctext);
-                    // DOM 객체에 접근하지 않기 위해 미리 데이터를 생성함.
-                    table.metadata[i].push({
-                        colspan: Number(cell.colSpan),
-                        rowspan: Number(cell.rowSpan)
-                    });
+                    ret.rows[i].push.apply(ret.rows[i], rtail);
+                    ret.metadata[i].push.apply(ret.metadata[i], mtail);
+                }
+
+                if (span.rowspan > 1) {
+                    // `rowspan` 개수만큼 셀에 데이터를 복사한다.
+                    for (var j = 1; j < span.rowspan; j++) {
+
+                        rtail = ret.rows[i + j].splice(k);
+                        mtail = ret.metadata[i + j].splice(k);
+
+                        ret.rows[i + j].push(fill);
+                        ret.metadata[i + j].push({type: ret.rows[i][k].nodeName, colspan: 0, rowspan: 0});
+
+                        ret.rows[i + j].push.apply(ret.rows[i + j], rtail);
+                        ret.metadata[i + j].push.apply(ret.metadata[i + j], mtail);
+                    }
                 }
             }
         }
+        return ret;
     }
 
-    // `colspan`, `rowspan` 데이터를 이용하여 빈 공간을 채움.
-    for (var i = 0; i < table.metadata.length; i++) {
+    // 테이블에 정보를 담는다.
+    var table = gather(element, options);
 
-        for (var k = 0; k < table.metadata[i].length; k++) {
-
-            var fill = options.mergeCell.fill === true ? table.rows[i][k] : options.mergeCell.fill;
-            var span = table.metadata[i][k];
-
-            if (span.colspan > 1) {
-                // `colspan` 개수만큼 셀에 데이터를 복사한다.
-                var rtail = table.rows[i].splice(k + 1);
-                var mtail = table.metadata[i].splice(k + 1);
-
-
-                for (var j = 1; j < span.colspan; j++) {
-
-                    table.rows[i].push(fill);
-                    table.metadata[i].push({colspan: 0, rowspan: 0});
-                }
-
-                table.rows[i].push.apply(table.rows[i], rtail);
-                table.metadata[i].push.apply(table.metadata[i], mtail);
-            }
-
-            if (span.rowspan > 1) {
-                // `rowspan` 개수만큼 셀에 데이터를 복사한다.
-                for (var j = 1; j < span.rowspan; j++) {
-
-                    rtail = table.rows[i + j].splice(k);
-                    mtail = table.metadata[i + j].splice(k);
-
-                    table.rows[i + j].push(fill);
-                    table.metadata[i + j].push({colspan: 0, rowspan: 0});
-
-                    table.rows[i + j].push.apply(table.rows[i + j], rtail);
-                    table.metadata[i + j].push.apply(table.metadata[i + j], mtail);
-                }
-            }
-        }
-    }
+    this.update = function (element, options) {
+        // 테이블 정보를 갱신한다.
+        table = gather(element, options);
+        return this;
+    };
 
     this.toArray = function () {
         // 데이터를 배열로 리턴한다.
         return table.rows.slice();
     };
 
-    this.toCSVdata = function (rows) {
+    this.toCSVdata = function (rows, csvOptions) {
+        // 기본 값
+        csvOptions = csvOptions || {};
         // CSV 포맷으로 변경하여 리턴
         var data = '';
-        rows = rows || table.rows;
+        // `rows` 데이터를 복사한다.
+        rows = (rows || table.rows).slice();
         for (var i = 0, ilen = rows.length, row; row = rows[i], i < ilen; i++) {
 
             for (var k = 0, klen = row.length, cell; cell = row[k], k < klen; k++) {
@@ -123,6 +157,7 @@ function TableExport(element, options) {
             }
             data += '\r\n';
         }
+        console.log(data);
         return 'data:text/csv;charset=utf-8,\uFEFF' + encodeURIComponent(data);
     };
 
@@ -135,4 +170,4 @@ function TableExport(element, options) {
         // 지원여부
         return 'download' in anchor;
     }
-}
+};
